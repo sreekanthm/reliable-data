6356c918e94a6b310f7c1def581e77db4e54c2c3
sreekanth <sreekanth.kanth@gmail.com>
2019-01-22 13:53:15 +0530

    Update NewTest.java

--- src/test/java/com/Introlligent/app/NewTest.java
+++ src/test/java/com/Introlligent/app/NewTest.java
@@ -1,32 +1,27 @@
 
-package tests;
- 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
- 
-public class NewTest {
- 
- public WebDriver driver;
- 
-  @Test
-  public void openMyBlog() {
- driver.get("https://www.softwaretestingmaterial.com/");
-  }
-  
-  @BeforeClass
-  public void beforeClass() {
-   
-   System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
-   driver = new FirefoxDriver();
-   
-  }
- 
-  @AfterClass
-  public void afterClass() {
-   driver.quit();
-  }
- 
-}
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
