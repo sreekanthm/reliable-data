34d7bea408a21b0be2b059a28321b40089b4de4b
sreekanth <sreekanth.kanth@gmail.com>
2019-01-23 12:35:06 +0530

    Update NewTest.java

--- src/test/java/com/Introlligent/app/NewTest.java
+++ src/test/java/com/Introlligent/app/NewTest.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebDriver;
  
 import org.openqa.selenium.chrome.ChromeDriver;
  
-public class TestChrome {
+public class NewTest {
  
 public static void main(String[] args) {
  
